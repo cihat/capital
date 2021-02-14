6c5757034b5f81bcaca79f26494946c3de44696b
cihatdev <cihat02020202@gmail.com>
2021-02-15 02:59:47 +0300

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,66 @@
-const loadMap = (position) => {
+const button = document.querySelector("#button");
+const input = document.querySelector("#city-input");
+const answerText = document.querySelector("#answerText");
+
+button.addEventListener("click", () => {
+  const inputValue = input.value;
+  getCountryData(`${inputValue}`);
+});
+
+const logCountry = (data) => {
+  console.log(data.latlng);
+  console.log(data.capital);
+  answerText.textContent = data.capital;
+  loadMap(data.latlng);
+};
+
+const getCountryData = (country) => {
+  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then((response) => {
+      // console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      // console.log([data]);
+      logCountry(data[0]);
+    });
+};
+
+// console.log(getCountryData("Turkey"));
+
+const loadMap2 = (position) => {
   const { latitude } = position.coords;
   const { longitude } = position.coords;
+  console.log(position);
   console.log(latitude, longitude);
   console.log(position);
-
   // leaflet
   var map = L.map("map").setView([latitude, longitude], 6);
-
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
-
   L.marker([latitude, longitude])
     .addTo(map)
     .bindPopup("You're here")
     .openPopup();
 };
 
+const loadMap = (position) => {
+  console.log(position);
+  var map = L.map("map").setView(position, 6);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker(position).addTo(map).bindPopup("You're here").openPopup();
+};
+
 const getPosition = () => {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(loadMap),
+    navigator.geolocation.getCurrentPosition(loadMap2),
       () => {
         alert(`Could not get your position`);
       };
@@ -28,3 +68,5 @@ const getPosition = () => {
 };
 
 getPosition();
+
+// loadMap2("");
