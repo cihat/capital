18591945e5f64d7e27d166c3d25f9dcad608ff6f
cihatdev <cihat02020202@gmail.com>
2021-02-15 01:20:52 +0300

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,61 @@
-var map = L.map("map").setView([51.505, -0.09], 13);
+// // geolocation api
+// const getPosition = () => {
+//   if (navigator.geolocation)
+//     navigator.geolocation.getCurrentPosition(loadMap(this), () => {
+//       alert(`Could not get your position`);
+//     });
+// };
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+// getPosition();
 
-L.marker([51.5, -0.09])
-  .addTo(map)
-  .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-  .openPopup();
+// const loadMap = (position) => {
+//   const { latitude } = position.coords;
+//   const { longitude } = position.coords;
+//   // console.log(latitude, longitude);
+//   console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+//   const coords = [latitude, longitude];
+
+//   this.#map = L.map("map").setView(coords, this.#mapZoomLevel);
+//   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//     attribution:
+//       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//   }).addTo(this.#map);
+
+//   // Handling clicks on map
+//   this.#map.on("click", this._showForm.bind(this));
+
+//   this.#workouts.forEach((work) => {
+//     this._renderWorkoutMarker(work);
+//   });
+// };
+
+const loadMap = (position) => {
+  const { latitude } = position.coords;
+  const { longitude } = position.coords;
+  console.log(latitude, longitude);
+  console.log(position);
+
+  // leaflet
+  var map = L.map("map").setView([latitude, longitude], 5);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker([latitude, longitude])
+    .addTo(map)
+    .bindPopup("You're here")
+    .openPopup();
+};
+
+const getPosition = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(loadMap),
+      () => {
+        alert(`Could not get your position`);
+      };
+  }
+};
+
+getPosition();
