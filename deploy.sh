0d54ab19614a61e0c1e610cba3910e51f7736b91
cihat <cihat02020202@gmail.com>
2021-04-18 14:39:43 +0300

    update repository name

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:cihat/capitals.git master:gh-pages
+git push -f git@github.com:cihat/capital.git master:gh-pages
 cd -
